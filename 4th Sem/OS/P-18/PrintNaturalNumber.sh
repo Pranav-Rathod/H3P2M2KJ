33dcfff4b031ab7996c918acf8d38d08f22bf62c
Pranav-Rathod <pranav.b.rathod@gmail.com>
2021-04-12 15:12:58 +0000

    Added 2 Precticals

---
+++ 4th Sem/OS/P-18/PrintNaturalNumber.sh
@@ -0,0 +1,7 @@
+echo -n "Enter Last Number : "
+read in
+
+for((i=1;i<=$in;i++))
+{
+    echo $i
+}
---
+++ 4th Sem/OS/P-20/PrintNaturalNumberReverse.sh
@@ -0,0 +1,7 @@
+echo -n "Enter Last Number : "
+read in
+
+for((i=$in;i>=1;i--))
+{
+    echo $i
+}
